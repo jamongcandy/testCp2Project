3ada3b0de10dfdd03ae4fdfa1f41c239ad20ac36
unknown <jkkissthesky@gmail.com>
2016-11-24 10:09:34 +0900

    Frame class, ExArray class implement, practice Template

--- thread/thread/main.cpp
+++ thread/thread/main.cpp
@@ -1,6 +1,9 @@
+#pragma once
 #include <process.h>
 #include <iostream>
 #include "KeyEvent.h"
+#include "frame.h"
+#include "ExArray.h"
 using namespace std;
 
 unsigned int __stdcall mythread(void*) {
@@ -28,17 +31,67 @@ unsigned int __stdcall keyEvent(void*) {
 }
 
 
-int main(void) {
-	HANDLE handleA, handleB;
+//int main(void) {
+//	HANDLE handleA, handleB;
+//
+//	handleA = (HANDLE)_beginthreadex(0, 0, &mythread, (void*)0, 0, 0);
+//	handleB = (HANDLE)_beginthreadex(0, 0, &keyEvent, (void*)0, 0, 0);
+//
+//	WaitForSingleObject(handleA, INFINITE);
+//	WaitForSingleObject(handleB, INFINITE);
+//
+//	CloseHandle(handleA);
+//	CloseHandle(handleB);
+//
+//	return 0;
+//}
 
-	handleA = (HANDLE)_beginthreadex(0, 0, &mythread, (void*)0, 0, 0);
-	handleB = (HANDLE)_beginthreadex(0, 0, &keyEvent, (void*)0, 0, 0);
+template<typename T>
+void mySwap(T& num1, T& num2) {
+	T temp = num1;
+	num1 = num2;
+	num2 = temp;
+}
 
-	WaitForSingleObject(handleA, INFINITE);
-	WaitForSingleObject(handleB, INFINITE);
 
-	CloseHandle(handleA);
-	CloseHandle(handleB);
+void main() {
+	try {
+		Frame *f1 = new Frame;
+		Frame* f2 = new Frame[10];
 
-	return 0;
+		delete f1;
+		delete[] f2;
+	}
+	catch (string msg) {
+		cout << "Error msg: " << msg << endl;
+	}
+
+	ExArray<int> arr1(3);
+	ExArray<char>arr2(4);
+	ExArray<double>arr3(5);
+
+	arr1.addData(2);
+	arr2.addData('A');
+	arr3.addData(32.12);
+
+	arr1.printData();
+	arr2.printData();
+	arr3.printData();
+
+	int num1 = 10, num2 = 40;
+	cout << "before : " << num1 << ", " << num2 << endl;
+	mySwap<int>(num1, num2);
+	cout << "after : " << num1 << ", " << num2 << endl;
+
+	double num1d = 121.11, num2d = 10.5;
+	cout << "before : " << num1d << ", " << num2d << endl;
+	mySwap<double>(num1d, num2d);
+	cout << "after : " << num1d << ", " << num2d << endl;
+
+	ExArray<int> arr4(5);
+	arr4.addData(10);
+
+	mySwap<ExArray<int>>(arr1, arr4);
+	arr1.printData();
+	arr4.printData();
 }
