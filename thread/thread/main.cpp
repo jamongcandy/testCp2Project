aca0e5ca4820fd730f34d4eae91f6775a86a1cec
unknown <jkkissthesky@gmail.com>
2016-12-01 09:38:24 +0900

    STL vector practice

--- thread/thread/main.cpp
+++ thread/thread/main.cpp
@@ -4,6 +4,8 @@
 #include "KeyEvent.h"
 #include "frame.h"
 #include "ExArray.h"
+#include <string>
+#include <vector>
 using namespace std;
 
 unsigned int __stdcall mythread(void*) {
@@ -54,44 +56,69 @@ void mySwap(T& num1, T& num2) {
 }
 
 
+//void main() {
+//	try {
+//		Frame *f1 = new Frame;
+//		Frame* f2 = new Frame[10];
+//
+//		delete f1;
+//		delete[] f2;
+//	}
+//	catch (string msg) {
+//		cout << "Error msg: " << msg << endl;
+//	}
+//
+//	ExArray<int> arr1(3);
+//	ExArray<char>arr2(4);
+//	ExArray<double>arr3(5);
+//
+//	arr1.addData(2);
+//	arr2.addData('A');
+//	arr3.addData(32.12);
+//
+//	arr1.printData();
+//	arr2.printData();
+//	arr3.printData();
+//
+//	int num1 = 10, num2 = 40;
+//	cout << "before : " << num1 << ", " << num2 << endl;
+//	mySwap<int>(num1, num2);
+//	cout << "after : " << num1 << ", " << num2 << endl;
+//
+//	double num1d = 121.11, num2d = 10.5;
+//	cout << "before : " << num1d << ", " << num2d << endl;
+//	mySwap<double>(num1d, num2d);
+//	cout << "after : " << num1d << ", " << num2d << endl;
+//
+//	ExArray<int> arr4(5);
+//	arr4.addData(10);
+//
+//	mySwap<ExArray<int>>(arr1, arr4);
+//	arr1.printData();
+//	arr4.printData();
+//}
+
 void main() {
-	try {
-		Frame *f1 = new Frame;
-		Frame* f2 = new Frame[10];
+	vector<int> v;
 
-		delete f1;
-		delete[] f2;
+	v.push_back(10);
+	v.push_back(20);
+	v.push_back(30);
+	v.insert(v.begin(), 40);
+	v.insert(v.end(), 50);
+
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << endl;
 	}
-	catch (string msg) {
-		cout << "Error msg: " << msg << endl;
+	cout << "access index at 1 : " << v.at(1) << endl;
+	v.erase(v.begin());
+	v.erase(v.end() - 1);
+
+	vector<int>::iterator iter = v.begin();
+	cout << "access index at 1 with iterator : " << iter[1] << endl;
+
+	while (iter != v.end()) {
+		cout << *iter << endl;
+		iter++;
 	}
-
-	ExArray<int> arr1(3);
-	ExArray<char>arr2(4);
-	ExArray<double>arr3(5);
-
-	arr1.addData(2);
-	arr2.addData('A');
-	arr3.addData(32.12);
-
-	arr1.printData();
-	arr2.printData();
-	arr3.printData();
-
-	int num1 = 10, num2 = 40;
-	cout << "before : " << num1 << ", " << num2 << endl;
-	mySwap<int>(num1, num2);
-	cout << "after : " << num1 << ", " << num2 << endl;
-
-	double num1d = 121.11, num2d = 10.5;
-	cout << "before : " << num1d << ", " << num2d << endl;
-	mySwap<double>(num1d, num2d);
-	cout << "after : " << num1d << ", " << num2d << endl;
-
-	ExArray<int> arr4(5);
-	arr4.addData(10);
-
-	mySwap<ExArray<int>>(arr1, arr4);
-	arr1.printData();
-	arr4.printData();
 }
