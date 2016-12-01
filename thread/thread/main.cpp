909a92d06858fce213dd98b572e3de942eb4b0fb
unknown <jkkissthesky@gmail.com>
2016-12-01 10:02:03 +0900

    STL practice ok

--- thread/thread/main.cpp
+++ thread/thread/main.cpp
@@ -6,31 +6,33 @@
 #include "ExArray.h"
 #include <string>
 #include <vector>
+#include <list>
+#include <map>
 using namespace std;
 
-unsigned int __stdcall mythread(void*) {
-	int i = 0;
-	while (1) {
-		cout << GetCurrentThreadId() << ": " << i++ << endl;
-		Sleep(500);
-	}
-	return 0;
-}
+//unsigned int __stdcall mythread(void*) {
+//	int i = 0;
+//	while (1) {
+//		cout << GetCurrentThreadId() << ": " << i++ << endl;
+//		Sleep(500);
+//	}
+//	return 0;
+//}
 
-unsigned int __stdcall keyEvent(void*) {
-	KeyEvent k;
-	int i;
-	while (1) {
-		i = k.getkey();
-		switch (i) {
-		case 37: cout << "Left key is pressed" << endl; break;
-		case 38: cout << "Up key is pressed" << endl; break;
-		case 39: cout << "Right key is pressed" << endl; break;
-		case 40: cout << "Down key is pressed" << endl; break;
-		}
-	}
-	return 0;
-}
+//unsigned int __stdcall keyEvent(void*) {
+//	KeyEvent k;
+//	int i;
+//	while (1) {
+//		i = k.getkey();
+//		switch (i) {
+//		case 37: cout << "Left key is pressed" << endl; break;
+//		case 38: cout << "Up key is pressed" << endl; break;
+//		case 39: cout << "Right key is pressed" << endl; break;
+//		case 40: cout << "Down key is pressed" << endl; break;
+//		}
+//	}
+//	return 0;
+//}
 
 
 //int main(void) {
@@ -48,12 +50,12 @@ unsigned int __stdcall keyEvent(void*) {
 //	return 0;
 //}
 
-template<typename T>
-void mySwap(T& num1, T& num2) {
-	T temp = num1;
-	num1 = num2;
-	num2 = temp;
-}
+//template<typename T>
+//void mySwap(T& num1, T& num2) {
+//	T temp = num1;
+//	num1 = num2;
+//	num2 = temp;
+//}
 
 
 //void main() {
@@ -98,9 +100,30 @@ void mySwap(T& num1, T& num2) {
 //	arr4.printData();
 //}
 
-void main() {
-	vector<int> v;
+void dump(list<string> &l) {
+	list<string>::iterator iter = l.begin();
+	while (iter != l.end()) {
+		cout << *iter << endl;
+		iter++;
+	}
+}
 
+class Word {
+private:
+	string word;
+public:
+	Word(string word) {
+		this->word = word;
+	}
+	string getWord() {
+		return this->word;
+	}
+};
+
+void main() {
+	//Vector
+	cout << "VECTOR=================" << endl;
+	vector<int> v;
 	v.push_back(10);
 	v.push_back(20);
 	v.push_back(30);
@@ -121,4 +144,55 @@ void main() {
 		cout << *iter << endl;
 		iter++;
 	}
+
+	//List
+	cout << "LIST===================" << endl;
+	list<string> names;
+	names.insert(names.begin(), "Konkuk");
+	names.insert(names.end(), "University");
+	names.insert(names.end(), "SCLAB");
+	names.insert(names.end(), "CSY");
+	dump(names);
+	cout << "===" << endl;
+	names.reverse();
+	dump(names);
+
+	//Map
+	cout << "MAP====================" << endl;
+	map<string, int> m;
+	m["seoul"] = 100;
+	m["daegu"] = 120;
+	m["busan"] = 200;
+
+	cout << "Train to Seoul = $" << m["seoul"] << endl;
+	cout << "Train to Daegu = $" << m["daegu"] << endl;
+	cout << "Train to Busan = $" << m["busan"] << endl;
+
+	map<char, list<Word> > m2;
+	list<Word> aList;
+	list<Word> bList;
+
+	aList.push_back(Word("apple"));
+	aList.push_back(Word("avoid"));
+	bList.push_back(Word("bread"));
+	bList.push_back(Word("bring"));
+
+	m2['a'] = aList;
+	m2['b'] = bList;
+	map<char, list<Word>>::const_iterator m2_iter = m2.begin();
+
+	while (m2_iter != m2.end()) {
+		cout << m2_iter->first << ":" << endl;
+		list<Word> temp = m2_iter->second;
+		list<Word>::const_iterator iter_list = temp.begin();
+
+		while (iter_list != temp.end()) {
+			Word word = *iter_list;
+			cout << word.getWord() << ", ";
+			iter_list++;
+		}
+		cout << "\n===" << endl;
+		m2_iter++;
+	}
+
 }
